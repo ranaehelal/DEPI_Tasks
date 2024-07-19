0844048efb9a14d978bc99055143a56346999ed2
ranaehelal <115326882+ranaehelal@users.noreply.github.com>
2024-07-19 06:37:23 +0300

    Delete ِAdapter directory

--- ِAdapter/Cassette.java
+++
@@ -1,7 +0,0 @@
-// Old Implementation
-// Adaptee
-class Cassette implements Player {
-    public void playCassette(String oldTape) {
-        System.out.println("Playing cassette: " + oldTape);
-    }
-}
